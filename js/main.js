2baed11b063626619c97354e6643812934f3f5cd
Julie Vikre <julievikre@gmail.com>
2019-11-06 18:05:57 -0500

    add map and directions

--- js/main.js
+++ js/main.js
@@ -3,6 +3,43 @@
  	easing: 'slide'
  });
 
+ function initMap() {
+	 // map options
+	 var options = {
+		zoom: 12,
+		center: {lat:40.054522, lng:-83.000063}
+	 }
+	 // new map
+	 var map = new google.maps.Map(document.getElementById('map'), options);
+	 // add marker
+	 var marker = new google.maps.Marker({ 
+		 map:map, 
+		 animation: google.maps.Animation.DROP,
+		 position:{lat:40.054522, lng:-83.000063}
+	});
+   }
+
+
+
+
+ $(document).ready(function(){
+	$(window).scroll(function () {
+			if ($(this).scrollTop() > 50) {
+				$('#back-to-top').fadeIn();
+			} else {
+				$('#back-to-top').fadeOut();
+			}
+		});
+		// scroll body to 0px on click
+		$('#back-to-top').click(function () {
+			$('body,html').animate({
+				scrollTop: 0
+			}, 400);
+			return false;
+		});
+});
+
+
 (function($) {
 
 	"use strict";
@@ -196,6 +233,52 @@
 	};
 	scrollWindow();
 
+	// Cache selectors
+var lastId,
+topMenu = $("#top-menu"),
+topMenuHeight = topMenu.outerHeight()+15,
+// All list items
+menuItems = topMenu.find("a"),
+// Anchors corresponding to menu items
+scrollItems = menuItems.map(function(){
+  var item = $($(this).attr("href"));
+  if (item.length) { return item; }
+});
+
+// Bind click handler to menu items
+// so we can get a fancy scroll animation
+menuItems.click(function(e){
+var href = $(this).attr("href"),
+  offsetTop = href === "#" ? 0 : $(href).offset().top-topMenuHeight+1;
+$('html, body').stop().animate({ 
+  scrollTop: offsetTop
+}, 300);
+e.preventDefault();
+});
+
+// Bind to scroll
+$(window).scroll(function(){
+// Get container scroll position
+var fromTop = $(this).scrollTop()+topMenuHeight;
+
+// Get id of current scroll item
+var cur = scrollItems.map(function(){
+ if ($(this).offset().top < fromTop)
+   return this;
+});
+// Get the id of the current element
+cur = cur[cur.length-1];
+var id = cur && cur.length ? cur[0].id : "";
+
+if (lastId !== id) {
+   lastId = id;
+   // Set/remove active class
+   menuItems
+	 .parent().removeClass("active")
+	 .end().filter("[href='#"+id+"']").parent().addClass("active");
+}                   
+});
+
 	var isMobile = {
 		Android: function() {
 			return navigator.userAgent.match(/Android/i);
@@ -337,12 +420,12 @@
     fixedContentPos: false
   });
 
-  $('.appointment_date').datepicker({
+  $('.contact_date').datepicker({
 	  'format': 'm/d/yyyy',
 	  'autoclose': true
 	});
 
-	$('.appointment_time').timepicker();
+	$('.contact_time').timepicker();
 
 
 })(jQuery);
